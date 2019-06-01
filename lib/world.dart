fa0207408ae7c726144051ced5d682050f1a2762
laurin-d <laurin.doerre@lgs-hu.eu>
2019-06-01 13:39:25 +0200

    added routes

---
+++ lib/world.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class World extends StatefulWidget {
+  @override
+  _WorldState createState() => _WorldState();
+}
+
+class _WorldState extends State<World> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
