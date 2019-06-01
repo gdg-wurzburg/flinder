ecc5264820b75e14fa38bfb2b5d6887ba3783e48
laurin-d <laurin.doerre@lgs-hu.eu>
2019-06-01 17:07:33 +0200

    kugel dreht sich

--- lib/world.dart
+++ lib/world.dart
@@ -1,9 +1,11 @@
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flinder/world/anicontrol.dart';
 import 'package:flinder/world/anim.dart';
 import 'package:flinder/world/location_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:provider/provider.dart';
+import 'package:location/location.dart';
 
 // ignore: must_be_immutable
 class World extends StatefulWidget {
@@ -12,18 +14,33 @@ class World extends StatefulWidget {
     Anim('lat', -90, 90, 1, false),
     Anim('lon', -180, 180, 1, true),
   ]);
-
   @override
   _WorldState createState() => _WorldState();
 }
 
 class _WorldState extends State<World> {
+  int map = 0;
+  AniControl controller;
+  String animationName = 'idle';
+
+  @override
+  void initState() {
+    controller = AniControl([
+      Anim('dir', 0, 360, 20, true),
+      Anim('lat', -90, 90, 1, false),
+      Anim('lon', -180, 180, 1, true),
+    ]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    for (int i = 0;i<360;i++){
-      widget.earth['lon'].value = i.toDouble();
-    }
-    return FlareActor("assets/earth.flr",
-        animation: 'pulse', color: Colors.blue, controller: widget.earth);
+    var actor = FlareActor("assets/earth.flr",
+        animation: animationName);
+    return GestureDetector(child: actor, onTap: (){
+      setState(() {
+        animationName = 'lon';
+      });
+    },);
   }
 }
